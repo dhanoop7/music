62842a55c01a2c00d1685beb7f068e611fde5c41
dhanoop7 <dhanoopdharan@gmail.com>
2024-02-27 00:02:51 +0530

    fourth commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 import { AiFillClockCircle } from 'react-icons/ai';
 import { useStateProvider } from '../utils/StateProvider';
 import axios from 'axios';
+import { reducerCases } from '../utils/Constants';
 
 const Container = styled.div``;
 
 export default function Body() {
-  const [{ token, selectedPlaylistsId }, dispatch] = useStateProvider();
+  const [{ token, selectedPlaylistsId, selectedPlaylist }, dispatch] = useStateProvider();
 
   useEffect(() => {
     const getInitialPlaylists = async () => {
@@ -18,7 +19,25 @@ export default function Body() {
             'Content-Type': 'application/json',
           },
         });
+        const selectedPlaylistData = {
+          id: response.data.id,
+          name: response.data.name,
+          description: response.data.description.startsWith("<a")
+            ? ""
+            : response.data.description,
+          image: response.data.images[0].url,
+          tracks: response.data.tracks.items.map(({ track }) => ({
+            id: track.id,
+            name: track.name,
+            artists: track.artists.map((artist) => artist.name),
+            image: track.album.name,
+            context_uri: track.album.uri,
+            track_number: track.track_number,
+          })),
+        };
+        console.log(selectedPlaylistData);
         console.log(response.data); // Log the playlist data
+        dispatch({ type: reducerCases.SET_PLAYLIST, selectedPlaylist: selectedPlaylistData });
       } catch (error) {
         console.error('Error fetching initial playlists:', error);
       }
@@ -27,5 +46,75 @@ export default function Body() {
     getInitialPlaylists();
   }, [token, selectedPlaylistsId, dispatch]);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      {selectedPlaylist && (
+        <>
+        <div className="playlist">
+          <div className="image">
+            <img src={selectedPlaylist.image} alt="selectedplaylist" />
+          </div>
+          <div className="details">
+            <span className='type'>PLAYLIST</span>
+            <h1 className='title'>{selectedPlaylist.name}</h1>
+            <p className='description'>{selectedPlaylist.description}</p>
+          </div>
+        </div>
+        <div className="list">
+          <div className="header_row">
+          <div className="col">
+              <span>#</span>
+            </div>
+            <div className="col">
+              <span>TITLE</span>
+            </div>
+            <div className="col">
+              <span>ALBUM</span>
+            </div>
+            <div className="col">
+              <span><AiFillClockCircle/></span>
+            </div>
+          </div>
+          <div className="tracks">
+            {
+              selectedPlaylist.tracks.map(({
+                id,
+                name,
+                artists,
+                image,
+                duration,
+                album,
+                context_uri,
+                track_number,
+              },index)=>{
+                return(
+                  <div className="row" key={id}>
+                    <div className="col">
+                      <span>{index+1}</span>
+                    </div>
+                    <div className="col detail">
+                      <div className="image"><img src={image} alt='track' /></div>
+                      <div className="info">
+                      <span className="name">{name}</span>
+                      <span>{artists}</span>
+                    </div>
+                    </div>
+                    <div className="col">
+                      <span>{album}</span>
+                    </div>
+                    <div className="col">
+                      <span>{duration}</span>
+                    </div>
+                  
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        </>
+      )}
+    </Container>
+  );
 }
+
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -5,6 +5,7 @@ export const initialState = {
     playlists: [],
     userInfo: null,
     selectedPlaylistsId: "37i9dQZF1DX6bnzK9KPvrz",
+    selectedPlaylist: null,
 }
 
 const reducer = (state, action) => {
@@ -25,6 +26,12 @@ const reducer = (state, action) => {
             return{
                 ...state,
                 userInfo: action.userInfo,
+            }
+        }
+        case reducerCases.SET_PLAYLIST: {
+            return{
+                ...state,
+                selectedPlaylist: action.selectedPlaylist,
             }
         }
         default:
